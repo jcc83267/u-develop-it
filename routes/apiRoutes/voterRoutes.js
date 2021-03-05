8e9dfea7e2a930f29c55b1cbdc48a6997f5f2d31
James Canlas <jamescanlasjr@yahoo.com>
2021-03-04 18:37:29 -0800

    modularize database and added voter table and api routes

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/database');
+const inputCheck = require('../../utils/inputCheck');
+
+//get all voters
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+    const params = [];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+//
+
+//get single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+//
+module.exports = router;
